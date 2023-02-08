9bc77b7fc5aa2cf687bda2ad90a6bc1e8510533a
RainZhang <1146223936@qq.com>
2023-02-08 21:29:12 +0800

    java is studying too

--- javacode/Hello.java
+++ javacode/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("zzy is studying java");
+        System.out.println("zzy is studying java, today");
     }
 }
