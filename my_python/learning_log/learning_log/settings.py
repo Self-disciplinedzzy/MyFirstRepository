d853511998c9078c4e52d07ecae1b74bdea46882
Self-disciplinedzzy <1146223936@qq.com>
2024-09-30 22:35:02 +0800

    learning_log_project

--- my_python/learning_log/learning_log/settings.py
+++ my_python/learning_log/learning_log/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my application
+    'learning_logs',
+
+    # default application
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- my_python/learning_log/learning_logs/models.py
+++ my_python/learning_log/learning_logs/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Topic(models.Models):
+class Topic(models.Model):
     """用户学习主题"""
     text = models.CharField(max_length=200)
-    date_added = models.dateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """"返回字符串类型"""
