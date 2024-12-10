edb95466db4dc83b7685706ca908befef9e2cfd3
Self-disciplinedzzy <1146223936@qq.com>
2024-12-10 20:39:24 +0800

    clearing response

--- javacode/Hello.java
+++ javacode/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("zzy is studying java, today");
+        System.out.println("hello This is my first java_web class");
         System.out.println("2024-11-30T21:27:37");
     }
 }
