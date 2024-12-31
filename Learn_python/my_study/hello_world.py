4bd3597384a41242d9eeb03d425c356d0cc68b25
Self-disciplinedzzy <1146223936@qq.com>
2024-12-31 18:01:10 +0800

    python3 learning working

---
+++ Learn_python/my_study/hello_world.py
@@ -0,0 +1,18 @@
+# print("Hello Python World!")
+message = "Hello Python World"
+print(message)
+
+message = "Hello Python Crash Course world"
+print(message)
+
+# Python will only remember the latest value
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("=============")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello,{full_name.title()}!")
